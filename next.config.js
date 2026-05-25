a8a507fade7bc3b583b3d832214befe886afee5f
targetdan <shinetime.sg1@hotmail.co.uk>
2026-05-25 17:09:58 +0100

    static build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
+    images: {
+        // Disable Next.js optimizations for images (it's not available for static sites and will throw an error)
+        unoptimized: true,
+    },
+
+    // This tells Next.js to export a static build to the `out` folder
+    output: "export",
+
+    // This tells Next.js to export pages as "folders with an `index.html` file inside"
+    // We use this option so we can avoid having the `.html` extension at the end of the page URLs.
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
